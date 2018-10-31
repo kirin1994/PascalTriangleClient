913b9bf91d4ea0561fe57cfb1440d3bdb146a180
Adrian Podlawski <apodlawski@gmail.com>
2018-10-31 08:16:16 +0100

    first client push

--- src/App.js
+++ src/App.js
@@ -3,26 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:55976/api/pascal/calculate?NumberOfIterations=40&Modulo=3', {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          isLoaded: true,
+          items: json,
+          headers:
+          {
+            "Access-Control-Allow-Origin": "*"
+          },
+
+        })
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    var { isLoaded, items } = this.state
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+
+    else {
+      return (
+        <div className="triangle">
+          {items.TriangleRows.map(row => <div className="triangle-row"> {row.ValuesOfCalculation.map(value => <a className={"triangle-component" + value} > {value} </a>)}</div>)}
+        </div>
+      );
+    }
   }
 }
-
 export default App;
