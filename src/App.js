f2633ea1a5a29cdaa530465982622c373de9bd7a
Podlawski <adrian.podlawski@intel.com>
2018-11-02 01:01:16 +0100

    working triangle poc without clean code

--- src/App.js
+++ src/App.js
@@ -8,11 +8,36 @@ class App extends Component {
     this.state = {
       items: [],
       isLoaded: false,
+      moduloValue: 0,
+      triangleHeight: 0
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount() {
-    fetch('http://localhost:55976/api/pascal/calculate?NumberOfIterations=40&Modulo=3', {
+  handleChange(event)
+  {
+    console.log("zmieniam");
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+  }
+
+  handleSubmit(event)
+  {
+    console.log(event.target.moduloValue);
+    this.setState({moduloValue : event.target.moduloValue, triangleHeight : event.target.triangleHeight});
+    this.getTrojkat();
+    event.preventDefault();
+  }
+
+  getTrojkat() {
+    console.log("wszedlem" + this.state.moduloValue + this.state.triangleHeight);
+    if(this.state.moduloValue == 0 || this.state.triangleHeight == 0)
+    {
+      return;
+    }
+    fetch('http://localhost:55976/api/pascal/calculate?NumberOfIterations='+ this.state.triangleHeight+'&Modulo=' + this.state.moduloValue, {
       method: "GET",
       mode: "cors"
     })
@@ -31,17 +56,62 @@ class App extends Component {
       });
   }
 
+
+  componentDidMount() {
+    if(this.state.moduloValue == 0 || this.state.triangleHeight == 0)
+    {
+      return;
+    }
+    fetch('http://localhost:55976/api/pascal/calculate?NumberOfIterations='+ this.state.triangleHeight+'&Modulo=' + this.state.moduloValue, {
+      method: "GET",
+      mode: "cors"
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          isLoaded: true,
+          items: json,
+          headers:
+          {
+            "Access-Control-Allow-Origin": "*"
+          },
+
+        })
+      });
+  }
+
+
   render() {
 
-    var { isLoaded, items } = this.state
-    if (!isLoaded) {
-      return <div>Loading...</div>
+    var { isLoaded, items, moduloValue, triangleHeight } = this.state
+    if (moduloValue == 0 || triangleHeight == 0 || !isLoaded) {
+      return  (    
+      <form onSubmit={this.handleSubmit}>
+      <label>Modulo: 
+      <input type="text" name="moduloValue" value={this.state.moduloValue} onChange={this.handleChange}/>
+      </label>
+      <label>Triangle Height: 
+      <input type="text" name="triangleHeight" value={this.state.triangleHeight} onChange={this.handleChange}/>
+      </label>
+      <input type="submit" value="Submit"/>
+    </form>)
     }
 
-    else {
+    else if(items != []){
       return (
         <div className="triangle">
+  <form onSubmit={this.handleSubmit}>
+      <label>Modulo: 
+      <input type="text" name="moduloValue" value={this.state.moduloValue} onChange={this.handleChange}/>
+      </label>
+      <label>Triangle Height: 
+      <input type="text" name="triangleHeight" value={this.state.triangleHeight} onChange={this.handleChange}/>
+      </label>
+      <input type="submit" value="Submit"/>
+    </form>)
           {items.TriangleRows.map(row => <div className="triangle-row"> {row.ValuesOfCalculation.map(value => <a className={"triangle-component" + value} > {value} </a>)}</div>)}
+         
         </div>
       );
     }
