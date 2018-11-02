c2ba39ada33bf43136df898c34f09114e31fcf63
Podlawski <adrian.podlawski@intel.com>
2018-11-02 23:39:03 +0100

    update for request refresh and new input fields

--- src/App.js
+++ src/App.js
@@ -18,98 +18,92 @@ class App extends Component {
   handleChange(event)
   {
     console.log("zmieniam");
-  this.setState({
-    [event.target.name]: event.target.value
+    this.setState({
+    [event.target.name]: event.target.value,
+    isLoaded : false,
   })
   }
 
   handleSubmit(event)
   {
-    console.log(event.target.moduloValue);
-    this.setState({moduloValue : event.target.moduloValue, triangleHeight : event.target.triangleHeight});
     this.getTrojkat();
     event.preventDefault();
   }
 
   getTrojkat() {
-    console.log("wszedlem" + this.state.moduloValue + this.state.triangleHeight);
-    if(this.state.moduloValue == 0 || this.state.triangleHeight == 0)
-    {
-      return;
-    }
-    fetch('http://localhost:55976/api/pascal/calculate?NumberOfIterations='+ this.state.triangleHeight+'&Modulo=' + this.state.moduloValue, {
+    fetch('https://localhost:44307/api/pascal/calculate?NumberOfIterations='+ this.state.triangleHeight+'&Modulo=' + this.state.moduloValue, {
       method: "GET",
-      mode: "cors"
+      mode: "cors",
+      // headers:
+      // {
+      //   "Access-Control-Allow-Origin": "*"
+      // },
     })
-      .then(res => res.json())
+      .then(function(res)
+      {
+        console.log(res);
+        if (res.ok){
+        return res.json();
+      }
+       else {
+         throw Error(res.statusText)} 
+      })
       .then(json => {
         console.log(json);
         this.setState({
           isLoaded: true,
           items: json,
-          headers:
-          {
-            "Access-Control-Allow-Origin": "*"
-          },
-
         })
-      });
+      }).catch(error => console.log(error));
   }
-
-
-  componentDidMount() {
-    if(this.state.moduloValue == 0 || this.state.triangleHeight == 0)
-    {
-      return;
-    }
-    fetch('http://localhost:55976/api/pascal/calculate?NumberOfIterations='+ this.state.triangleHeight+'&Modulo=' + this.state.moduloValue, {
-      method: "GET",
-      mode: "cors"
-    })
-      .then(res => res.json())
-      .then(json => {
-        console.log(json);
-        this.setState({
-          isLoaded: true,
-          items: json,
-          headers:
-          {
-            "Access-Control-Allow-Origin": "*"
-          },
-
-        })
-      });
-  }
-
 
   render() {
 
     var { isLoaded, items, moduloValue, triangleHeight } = this.state
     if (moduloValue == 0 || triangleHeight == 0 || !isLoaded) {
-      return  (    
-      <form onSubmit={this.handleSubmit}>
-      <label>Modulo: 
-      <input type="text" name="moduloValue" value={this.state.moduloValue} onChange={this.handleChange}/>
-      </label>
-      <label>Triangle Height: 
-      <input type="text" name="triangleHeight" value={this.state.triangleHeight} onChange={this.handleChange}/>
-      </label>
-      <input type="submit" value="Submit"/>
-    </form>)
+      return  (  
+        <div className="triangle">
+        <div style={{width:300, alignContent: "center"}}>
+          <form onSubmit={this.handleSubmit}>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="basic-addon1">Modulo</span>
+              </div> 
+              <input type="text" aria-label="Modulo" aria-describedby="basic-addon1" className="form-control" name="moduloValue" value={this.state.moduloValue} onChange={this.handleChange}/>
+            </div>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="basic-addon1">Triangle Height</span>
+              </div> 
+              <input type="text" aria-label="Triangle Height" aria-describedby="basic-addon1" className="form-control" name="triangleHeight" value={this.state.triangleHeight} onChange={this.handleChange}/>
+            </div>
+            <input type="submit" className="form-control" value="Submit"/>
+           </form>
+           </div>
+        </div>)
     }
 
-    else if(items != []){
+    else if(items != [] && items != undefined){
       return (
         <div className="triangle">
-  <form onSubmit={this.handleSubmit}>
-      <label>Modulo: 
-      <input type="text" name="moduloValue" value={this.state.moduloValue} onChange={this.handleChange}/>
-      </label>
-      <label>Triangle Height: 
-      <input type="text" name="triangleHeight" value={this.state.triangleHeight} onChange={this.handleChange}/>
-      </label>
-      <input type="submit" value="Submit"/>
-    </form>)
+              <div style={{width:300, alignContent: "center"}}>
+          <form onSubmit={this.handleSubmit}>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="basic-addon1">Modulo</span>
+              </div> 
+              <input type="text" aria-label="Modulo" aria-describedby="basic-addon1" className="form-control" name="moduloValue" value={this.state.moduloValue} onChange={this.handleChange}/>
+            </div>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="basic-addon1">Triangle Height</span>
+              </div> 
+              <input type="text" aria-label="Triangle Height" aria-describedby="basic-addon1" className="form-control" name="triangleHeight" value={this.state.triangleHeight} onChange={this.handleChange}/>
+            </div>
+            <input type="submit" className="form-control"  value="Submit"/>
+           </form>
+           </div>
+           {console.log(items)}
           {items.TriangleRows.map(row => <div className="triangle-row"> {row.ValuesOfCalculation.map(value => <a className={"triangle-component" + value} > {value} </a>)}</div>)}
          
         </div>
